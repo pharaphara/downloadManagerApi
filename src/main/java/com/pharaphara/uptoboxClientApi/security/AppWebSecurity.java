c7185bce2c0ca02148c061074a98c2617f3e397f
Alexandre DELTOUR <alexandre.deltour@geodis.com>
2022-10-06 11:19:36 +0200

    filterChain

--- src/main/java/com/pharaphara/uptoboxClientApi/security/AppWebSecurity.java
+++ src/main/java/com/pharaphara/uptoboxClientApi/security/AppWebSecurity.java
@@ -12,36 +12,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class AppWebSecurity  {
 
-
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //log.info("username {}, ",username);
 
        http
                .csrf().disable()
                .authorizeHttpRequests()
                .anyRequest()
                .authenticated()
-
                .and()
                .httpBasic()
                ;
 
        return http.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        //log.info("username {}, ",username);
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        String encodedPassword = passwordEncoder().encode(password);
-//        manager.createUser(User.withUsername(username).password(encodedPassword).roles("USER").build());
-//        return manager;
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 }
